0d3a4380288284410b5cb9f46335ef5d1191dc68
HindMukhtar <62732638+HindMukhtar@users.noreply.github.com>
2026-04-08 07:53:35 -0400

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def recurse(): 
+        memo = {}
+        if len(nums) == 1: 
+            return nums[0]
+
+        def dfs(i): 
+            if i in memo: 
+                return memo[i]
+            if i == len(nums): 
+                return 0 
+            
+            memo[i] = max(nums[i], nums[i]+ dfs(i+1)) 
+
+            return memo[i]
+
+        best = 0 
+
+        for i in range(len(nums)): 
+            best = max(best, dfs(i))
+
+        return best 
+
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        prev_sum = nums[len(nums)-1]
+        best = prev_sum
+
+        for i in range(len(nums)-2, -1, -1):
+            curr_sum = max(nums[i], nums[i]+prev_sum) 
+            prev_sum = curr_sum
+            best = max(best, curr_sum)
+        
+        return best
