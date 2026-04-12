753710b12c3f9912fae6d96953c3953f325070a6
HindMukhtar <62732638+HindMukhtar@users.noreply.github.com>
2026-04-12 11:17:12 -0400

    Add: pow-x-n - submission-2

---
+++ Data Structures & Algorithms/pow-x-n/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def BF(): 
+        res = 1
+        pwr = abs(n)
+
+        while pwr > 0: 
+            res*=x 
+            pwr-=1 
+
+        return res if n>0 else (1/res)
+
+    def myPow(self, x: float, n: int) -> float:
+        def fast_pow(x, y):
+            if y == 0: 
+                return 1 
+            if y >= 1: 
+                ans = fast_pow(x, y//2)
+                if y%2 == 0: 
+                    return ans*ans
+                else: 
+                    return ans*ans*x 
+        
+        pwr = abs(n)
+
+        res = fast_pow(x, pwr)
+
+        return res if n>0 else (1/res)
